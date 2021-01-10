24dc9c700c656462e484b56313486782de86132e
SOUnit <workbookwork7@gmail.com>
2021-01-10 14:19:48 +0900

    added list pages

---
+++ wp-content/themes/sample/category.php
@@ -0,0 +1,29 @@
+<!-- header -->
+<?php get_header(); ?>
+
+<!-- menu -->
+<?php get_template_part('content', 'menu'); ?>
+    
+    <div id="main">
+
+        <!-- blog_list -->
+        <section id="blog_list" class="site-width">
+            <h1 class="title">BLOG</h1>
+            <div id="content" class="article">
+                
+                <!-- loop article -->
+                <?php get_template_part('loop'); ?>
+
+                <?php if(function_exists("pagination") pagination($additional_loop->max_num_pages)); ?>
+            
+            </div>
+
+            <!-- side bar -->
+            <?php get_sidebar(); ?>
+
+        </section>
+
+    </div>
+
+    <!-- footer -->
+    <?php get_footer(); ?>
