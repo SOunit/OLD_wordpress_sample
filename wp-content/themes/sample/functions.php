205313b493faffe9e20d4feba91af6336975ff15
SOUnit <workbookwork7@gmail.com>
2021-01-11 14:33:47 +0900

    added custom field in about section

--- wp-content/themes/sample/functions.php
+++ wp-content/themes/sample/functions.php
@@ -64,3 +64,51 @@ function pagination($pages = '', $range = 2)
         echo '</div>';
     }
 }
+
+// custom field
+// define custom field to show in post page
+// add_action(place, function);
+add_action('admin_menu', 'add_custom_inputbox');
+// action fook of item shown for insert and update
+add_action('save_post', 'save_custom_postdata');
+
+// decide which post type of page for custom box to show
+function add_custom_inputbox()
+{
+    /**
+     * add_meta_box(
+     *  1. id name for admin html page
+     *  2. custom field name in admin page
+     *  3. function name for meta box
+     *  4. custom field place in admin
+     *  5. order
+     * )
+     */
+    add_meta_box('about_id', 'About Input', 'custom_area', 'page', 'normal');
+}
+
+// show custom area in admin page
+function custom_area()
+{
+    global $post;
+
+    echo 'comment: <textarea col="50" row="5" name="about_msg">' . get_post_meta($post->ID, 'about', true) . '</textarea>';
+}
+
+// save and update when post button is clicked
+function save_custom_postdata($post_id)
+{
+    $about_msg = '';
+
+    // get data from custom field
+    if (isset($_POST['about_msg'])) {
+        $about_msg = $_POST['about_msg'];
+    }
+
+    // update if content changed
+    if ($about_msg != get_post_meta($post_id, 'about', true)) {
+        update_post_meta($post_id, 'about', $about_msg);
+    } elseif ($about_msg == '') {
+        delete_post_meta($post_id, 'about', get_post_meta($post_id, 'about', true));
+    }
+}
